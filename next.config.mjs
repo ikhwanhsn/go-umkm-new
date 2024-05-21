b1f307c311dec8a245d4d49ed00debe857600aef
ikhwanhsn <ikhwanulhusna111@gmail.com>
2024-05-21 10:43:58 +0700

    chore: setting domain vercel

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
+    domains: ["https://go-umkm-smg.vercel.app"],
     remotePatterns: [
       {
         protocol: "https",
